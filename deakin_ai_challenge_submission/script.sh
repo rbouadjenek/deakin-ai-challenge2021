c711290e48b41cfc9ca68ca8e1ec2b015f3507c3
Mohamed Reda Bouadjenek <rbouadjenek@gmail.com>
2020-12-08 14:56:14 +1100

    bug fixed

--- deakin_ai_challenge_submission/script.sh
+++ deakin_ai_challenge_submission/script.sh
@@ -2,7 +2,7 @@
 
 cd program/
 # CONDA
-conda create -n mypython3 python=3 
+conda create -n mypython3 python=3.8
 source activate mypython3 2> /dev/null > /dev/null
 conda install numpy  2> /dev/null > /dev/null
 conda install tensorflow 2> /dev/null > /dev/null
